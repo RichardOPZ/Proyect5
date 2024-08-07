4d4d9920c6164df54f24ae7e1a24e30f7bc340aa
RichardPZ <vorticose_ropz@hotmail.com>
2024-08-06 23:49:09 -0600

    x

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')
-st.header('Datos')
-hist_button = st.button('Construir histograma')  # crear un botón
+st.header('Datos de Autos')
+hist_button = st.button('construir histograma')  # crear un botón
 # Crea un botón para construir un gráfico de dispersion
 scatter_button = st.button('construir dispersion')
 if hist_button:  # al hacer clic en el botón
